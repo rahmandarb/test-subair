7e499757a84dd01a6f330edbb7f37aa5a3fe55f8
abdul rahman <abdulrahman1712@gmail.com>
2023-09-22 19:32:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <div className="video h-36 w-60">Video</div>
+    </>
+  )
+}
+
+export default App
